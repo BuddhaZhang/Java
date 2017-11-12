d7bbc0dfd59b3f383ce450e0c32b7d9d0f8414df
BuddhaZhang <2573328502@qq.com>
2017-11-12 20:02:55 +0800

    提交

---
+++ HomeWork/src/Company.java
@@ -0,0 +1,34 @@
+
+abstract class Employee{//抽象类
+	public abstract double earning();
+}
+//按年领薪水
+class YearWorker extends Employee{
+	public double earning() {
+		
+		return 120000;
+	}
+}
+//按月领薪水
+class MonthWorker extends Employee{
+	public double earning() {
+		
+		return 10000;
+	}
+}
+//按周领薪水
+class WeekWorker extends Employee{
+	public double earning() {
+		
+		return 2500;
+	}
+}
+public class Company {
+	public static void main(String[] args) {
+		Employee[] employee=new Employee[3];
+		employee[0]=new YearWorker();
+		employee[1]=new MonthWorker();
+		employee[2]=new WeekWorker();
+		System.out.println(employee[0].earning()*1+employee[1].earning()*12+employee[2].earning()*(365/7));
+	}
+}
